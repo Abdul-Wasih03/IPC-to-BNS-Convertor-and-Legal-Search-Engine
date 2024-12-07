fe0dab1451b63101f1b9464718bc7edea56794ff
Abdul-Wasih03 <abdul.wasih1403@gmail.com>
2024-12-07 18:31:39 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,20 @@
+require('dotenv').config();
 const port = 4000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+
 app.use(express.json());
 app.use(cors());
 
-// Database Connection with MongoDB
-mongoose.connect("mongodb+srv://abdulwasih03:5EtkOshCWE74KqkT@cluster0.j8pts.mongodb.net/Legal-Lookup?retryWrites=true&w=majority")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => {
-    console.error("MongoDB connection error: ", err);
-    process.exit(1);  // Exit process in case of a fatal error
-  });
+MONGODB_URI="mongodb+srv://abdulwasih03:5EtkOshCWE74KqkT@cluster0.j8pts.mongodb.net/Legal-Lookup?retryWrites=true&w=majority"
 
-// Catch uncaught exceptions and unhandled promise rejections to avoid crashes
-process.on('uncaughtException', (err) => {
-  console.error("Uncaught Exception: ", err);
-});
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.error("Unhandled Rejection: ", reason);
-});
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Root Route
 app.get("/", (req, res) => {
